67b0352586ccb2e60f47cce4d1454ab004ee0e4a
eyakhediri1-cyber <eyakhediri1@gmail.com>
2026-03-02 05:42:45 +0100

    feat: Ajout validation entité Article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -8,37 +8,49 @@ use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\Article;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\ArticleRepository;
+use App\Form\ArticleType;
+use Symfony\Component\HttpFoundation\Request;
 
 final class ArticleController extends AbstractController
 {
-    #[Route('/articles/nouveau', name: 'app_article_nouveau')]
-     public function nouveau(EntityManagerInterface $em): Response
-    {
+    #[Route('/article/nouveau', name: 'app_article_nouveau')]
+     public function nouveau(Request $request, EntityManagerInterface $em): Response
+{
     $article = new Article();
-    $article->setTitre('Mon premier article');
-    $article->setContenu('Ceci est le contenu de mon premier article créé avec Doctrine.');
-    $article->setAuteur('Étudiant');
-    $article->setDateCreation(new \DateTime());
-    $article->setPublie(true);
-
-    $em->persist($article);
-    $em->flush();
-
-    return new Response("Article créé avec l'id : " . $article->getId());
+    
+    // Création du formulaire
+    $form = $this->createForm(ArticleType::class, $article);
+    
+    // Traitement de la requête
+    $form->handleRequest($request);
+    
+    if ($form->isSubmitted() && $form->isValid()) {
+        $em->persist($article);
+        $em->flush();
+        
+        // Message flash de confirmation
+        $this->addFlash('success', 'Article créé avec succès !');
+        
+        return $this->redirectToRoute('app_articles');
     }
-    #[Route('/articles', name: 'app_articles')]
+    
+    return $this->render('article/nouveau.html.twig', [
+        'formulaire' => $form,
+    ]);
+}
+    #[Route('/article', name: 'app_articles')]
     public function index(ArticleRepository $articleRepository): Response
     {
     $articles = $articleRepository->findAll();
 
-    return $this->render('articles/index.html.twig', [
+    return $this->render('article/index.html.twig', [
         'articles' => $articles,
     ]);
     }
-    #[Route('/articles/{id}', name: 'app_article_detail', requirements: ['id' => '\d+'])]
+    #[Route('/article/{id}', name: 'app_article_detail', requirements: ['id' => '\d+'])]
     public function detail(Article $article): Response
     {
-    return $this->render('articles/detail.html.twig', [
+    return $this->render('article/detail.html.twig', [
         'article' => $article,
     ]);
     }
